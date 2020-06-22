2355d0e7b1f2c8b4f43118cebd6ca6c622d62915
jimmim <kalinin_av@list.ru>
2020-06-22 16:37:53 +0300

    rename Users->UserController

--- src/main/java/com/avito/controllers/simple/UserController.java
+++ src/main/java/com/avito/controllers/simple/UserController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Users {
+public class UserController {
 
     @GetMapping("/users")
     public String users() { return "users"; }
