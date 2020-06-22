ea0fbc7f1a19e437cab5e7d9c02a6055f93588fa
markitan1990 <maar201000@gmail.com>
2020-06-22 13:57:24 +0300

    Удалил стандартные конструкторы и использую ломбок аннотации @AllArgsConstructor @NoArgsConstructor

--- src/main/java/com/avito/models/Category.java
+++ src/main/java/com/avito/models/Category.java
@@ -1,12 +1,16 @@
 package com.avito.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "categorys")
 public class Category {
@@ -20,14 +24,4 @@ public class Category {
 
     @OneToMany(cascade = CascadeType.ALL)
     private Set<Category> subCategories = new HashSet<>();
-
-    public Category() {
-    }
-
-    public Category(long id, String name, Set<Category> subCategories) {
-        this.id = id;
-        this.name = name;
-        this.subCategories = subCategories;
-    }
-
 }
