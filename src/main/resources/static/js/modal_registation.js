b039a18bdfd6c60b3fa042713c886fda33f91e23
Vadim Karpov <ksvkvv1990@gmail.com>
2020-06-23 01:59:40 +0300

    modal reg

--- src/main/resources/static/js/modal_registation.js
+++ src/main/resources/static/js/modal_registation.js
@@ -1,31 +1,14 @@
+$("#open-modal-1").click(function(){
+    $("#modal-reg-1").modal('show');
+});
+$("#open-modal-2").click(function () {
+    $("#modal-reg-2").modal("show");
+});
+$("#open-modal-3").click(function () {
+    $("#modal-reg-3").modal("show");
+});
 
-let modal_1 = document.getElementById("modal-reg");
-let btn_reg_1 = document.getElementById("open-modal-reg");
-let span_1 = document.getElementsByClassName("close")[0];
-let btn_open = document.getElementById("btn-open");
 
-btn_reg_1.onclick = function() {
-    modal_1.style.display = "block";
-}
-
-span_1.onclick = function() {
-    modal_1.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target === modal_1) {
-        modal_1.style.display = "none";
-    }
-}
-btn_open.onclick = function () {
-    modal_1.style.display = "none";
-}
-
-let btn_reg = document.getElementById("btn-reg");
- btn_reg.onclick = function () {
-let div = "";
-div = "<a href='/logout'>Logout</a>"
-    $("#li-auth").html(div);
-}
 
 
 
