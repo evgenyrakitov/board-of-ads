dafe9381b069f1dda20ea582389a338a310e7995
Sidorenkomv <50841585+Sidorenkomv@users.noreply.github.com>
2020-06-23 08:48:54 +0300

    fix

--- src/main/java/com/avito/configs/security/SecurityConfig.java
+++ src/main/java/com/avito/configs/security/SecurityConfig.java
@@ -82,15 +82,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-
-                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "/login**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("/login")
-                .defaultSuccessUrl("/main_page").failureUrl("/login?error").permitAll()
-                .and().logout().logoutSuccessUrl("/").permitAll();
+                    .formLogin()
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/main_page")
+                    .failureUrl("/login?error")
+                    .permitAll()
+                .and()
+                    .logout()
+                    .logoutSuccessUrl("/")
+                    .permitAll()
+                .and()
+                    .csrf().disable();
 
 
         //Добавление фильтра в конфигурацию
