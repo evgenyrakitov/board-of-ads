fe7fc5d1a3625f288b6a8260c9c79c01db02c154
Vadim Karpov <ksvkvv1990@gmail.com>
2020-06-24 00:22:42 +0300

    modal reg добавлены запросы ajax из модальных окон

--- src/main/resources/static/js/modal_registation.js
+++ src/main/resources/static/js/modal_registation.js
@@ -8,6 +8,37 @@ $("#open-modal-3").click(function () {
     $("#modal-reg-3").modal("show");
 });
 
+$("#btn-reg").click(function (event) {
+    event.preventDefault();
+    $.ajax({
+        url: $("#add-reg-form").attr("action"),
+        method: "PUT",
+        data: $("#add-reg-form").serialize(),
+        //success: alert("OK")
+    });
+});
+$("#btn-open").click(function (event) {
+    event.preventDefault();
+    $.ajax({
+        url: $("#open-form-modal").attr("action"),
+        method: "POST",
+        data: $("#open-form-modal").serialize(),
+        success: function (user) {
+        $("#open-modal-1").html("<span>"+user.publicName+"</span>");
+        }
+    });
+});
+$("#btn-modal-3").click(function (event) {
+    event.preventDefault();
+    $.ajax({
+        url: $("#form-modal-3").attr("action"),
+        method: "POST",
+        data: $("#form-modal-3").serialize(),
+        success: function (user) {
+            alert("Ваш пароль - "+user.password);
+        }
+    });
+})
 
 
 
