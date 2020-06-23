0639d5d8898ed32d616010fbfe52cad07df79208
markitan1990 <maar201000@gmail.com>
2020-06-23 21:52:31 +0300

    Создал страничку posting.html - Cоздал PostingRestController и в нем метод с заглушкой для передачи файлов - добавил 3 тестовые картинки

---
+++ src/main/java/com/avito/controllers/rest/PostingRestController.java
@@ -0,0 +1,33 @@
+package com.avito.controllers.rest;
+
+import com.avito.models.Category;
+import com.avito.models.Images;
+import com.avito.models.Posting;
+import com.avito.models.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/posting")
+public class PostingRestController {
+    @GetMapping("/getPostingInfo")
+    public ResponseEntity<Posting> getPosting() {
+        Set<Images> img = new HashSet<>();
+        Posting posting = new Posting(
+                "Коттедж 400 м² на участке 3 сот.",
+                new Category(),
+                new User(),
+                "Коттедж на два хозяина. На первом этаже кухня, зал и туалет с душем, второй этаж три комнаты и туалет с ванной, третий этаж-две комнаты. Цокольный этаж с гаражом и комнатой. Готов к заселению, возможна долгосрочная аренда.",
+                "Коттедж",
+                img
+        );
+        posting.setPrice(123123L);
+        return new ResponseEntity<>(posting, HttpStatus.OK);
+    }
+}
