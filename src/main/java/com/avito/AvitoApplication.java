71c4c3b44255f4c01607c755293768b3e08d1516
Boris Muzychuk <muzychuk.boris@gmail.com>
2020-06-24 10:49:45 +0300

    using @PostConstruct for fill DB with initial data

--- src/main/java/com/avito/AvitoApplication.java
+++ src/main/java/com/avito/AvitoApplication.java
@@ -2,12 +2,14 @@ package com.avito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 public class AvitoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AvitoApplication.class, args).start();
+        SpringApplication.run(AvitoApplication.class, args);
     }
 
 }
--- src/main/java/com/avito/configs/DataInitializer.java
+++ src/main/java/com/avito/configs/DataInitializer.java
@@ -4,11 +4,16 @@ import com.avito.models.Category;
 import com.avito.models.Posting;
 import com.avito.models.Role;
 import com.avito.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.ContextStartedEvent;
-import org.springframework.context.event.EventListener;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.Collections;
@@ -22,37 +27,50 @@ public class DataInitializer {
     @PersistenceContext
     private EntityManager entityManager;
 
-    @EventListener
-    public void handleContextStart(ContextStartedEvent event) {
+    @Autowired
+    @Qualifier("transactionManager")
+    protected PlatformTransactionManager txManager;
 
-        Role adminRole = new Role("ADMIN");
-        entityManager.persist(adminRole);
-        Role userRole = new Role("USER");
-        entityManager.persist(userRole);
+    /*
+     * If the code looks strange for you,
+     * then follow the link https://stackoverflow.com/questions/17346679/transactional-on-postconstruct-method/18790494#18790494
+     * I don't know how not to use transaction here.
+     * */
+    @PostConstruct
+    private void init() {
+        TransactionTemplate tmpl = new TransactionTemplate(txManager);
+        tmpl.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                Role adminRole = new Role("ADMIN");
+                entityManager.persist(adminRole);
+                Role userRole = new Role("USER");
+                entityManager.persist(userRole);
 
-        Set<Role> forAdmin = new HashSet<>();
-        forAdmin.add(adminRole);
-        forAdmin.add(userRole);
-        // For test, It must be delete in production code
-        entityManager.persist(new User("admin@gmail.com", "Test Admin name", "admin", "admin", forAdmin));
+                Set<Role> forAdmin = new HashSet<>();
+                forAdmin.add(adminRole);
+                forAdmin.add(userRole);
+                // For test, It must be delete in production code
+                entityManager.persist(new User("admin@gmail.com", "Test Admin name", "admin", "admin", forAdmin));
 
-        Set<Role> forUser = new HashSet<>();
-        forUser.add(userRole);
-        User user = new User("test.email.1@gmail.com", "test 1 public name", "qwerty1", "qwerty1", forUser);
-        // For test, It must be delete in production code
-        entityManager.persist(user);
+                Set<Role> forUser = new HashSet<>();
+                forUser.add(userRole);
+                User user = new User("test.email.1@gmail.com", "test 1 public name", "qwerty1", "qwerty1", forUser);
+                // For test, It must be delete in production code
+                entityManager.persist(user);
 
-        entityManager.persist(new Category("Недвижимость", Collections.emptySet()));
-        entityManager.persist(new Category("Работа", Collections.emptySet()));
-        entityManager.persist(new Category("Транспорт", Collections.emptySet()));
-        entityManager.persist(new Category("Услуги", Collections.emptySet()));
-        entityManager.persist(new Category("Личные вещи", Collections.emptySet()));
-        entityManager.persist(new Category("Для дома и дачи", Collections.emptySet()));
-        Category hobby = new Category("Хобби и отдых", Collections.emptySet());
-        entityManager.persist(hobby);
-        entityManager.persist(new Category("Животные", Collections.emptySet()));
-        generateStubPostings(user, hobby);
-
+                entityManager.persist(new Category("Недвижимость", Collections.emptySet()));
+                entityManager.persist(new Category("Работа", Collections.emptySet()));
+                entityManager.persist(new Category("Транспорт", Collections.emptySet()));
+                entityManager.persist(new Category("Услуги", Collections.emptySet()));
+                entityManager.persist(new Category("Личные вещи", Collections.emptySet()));
+                entityManager.persist(new Category("Для дома и дачи", Collections.emptySet()));
+                Category hobby = new Category("Хобби и отдых", Collections.emptySet());
+                entityManager.persist(hobby);
+                entityManager.persist(new Category("Животные", Collections.emptySet()));
+                generateStubPostings(user, hobby);
+            }
+        });
     }
 
     // For test, It must be delete in production code
