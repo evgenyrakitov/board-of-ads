39847487b48a7799dae3b81281c4447dd798e162
Boris Muzychuk <muzychuk.boris@gmail.com>
2020-06-24 11:53:56 +0300

    deletes unnecessary annotation

--- src/main/java/com/avito/AvitoApplication.java
+++ src/main/java/com/avito/AvitoApplication.java
@@ -2,9 +2,7 @@ package com.avito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableTransactionManagement
 @SpringBootApplication
 public class AvitoApplication {
 
