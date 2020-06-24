e07fb5fccfd68906c7b755d6a84b218069834033
Boris Muzychuk <muzychuk.boris@gmail.com>
2020-06-24 08:21:29 +0300

    adds public modifier

--- src/main/java/com/avito/controllers/simple/MainPageController.java
+++ src/main/java/com/avito/controllers/simple/MainPageController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainPageController {
 
     @GetMapping("/main_page")
-    String getMainPage() {
+    public String getMainPage() {
         return "index";
     }
 
     @GetMapping("/adDetails")
-    String getDetailsPage() {
+    public String getDetailsPage() {
         return "adDetails";
     }
 
