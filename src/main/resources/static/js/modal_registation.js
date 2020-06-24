9e79396e26540e92496b7bc0530aff3bd5035b10
Vadim Karpov <ksvkvv1990@gmail.com>
2020-06-24 16:13:34 +0300

    modal reg добавлены запросы ajax

---
+++ src/main/resources/static/js/modal_registation.js
@@ -0,0 +1,45 @@
+$("#open-modal-1").click(function(){
+    $("#modal-reg-1").modal('show');
+});
+$("#open-modal-2").click(function () {
+    $("#modal-reg-2").modal("show");
+});
+$("#open-modal-3").click(function () {
+    $("#modal-reg-3").modal("show");
+});
+
+$("#btn-reg").click(function (event) {
+    event.preventDefault();
+    $.ajax({
+        url: $("#add-reg-form").attr("action"),
+        method: "PUT",
+        data: $("#add-reg-form").serialize(),
+        success: alert("OK")
+    });
+});
+$("#btn-open").click(function (event) {
+    event.preventDefault();
+    $.ajax({
+        url: $("#open-form-modal").attr("action"),
+        method: "POST",
+        data: $("#open-form-modal").serialize(),
+        success: function (user) {
+        $("#open-modal-1").html("<span>"+user.publicName+"</span>");
+        }
+    });
+});
+$("#btn-modal-3").click(function (event) {
+    event.preventDefault();
+    $.ajax({
+        url: $("#form-modal-3").attr("action"),
+        method: "POST",
+        data: $("#form-modal-3").serialize(),
+        success: function (user) {
+            alert("Ваш пароль - "+user.password);
+        }
+    });
+})
+
+
+
+
