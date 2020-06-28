63829f9bc4a7a012262e38433e5fbac3ff2805e3
aleks <aleksandr.law@gmail.com>
2020-06-28 20:44:59 +0300

    начало заполненияе авто 280620 2044

--- src/main/java/com/avito/controllers/simple/CategoryController.java
+++ src/main/java/com/avito/controllers/simple/CategoryController.java
@@ -6,11 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
-    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+public class CategoryController {
+    private static final Logger logger = LoggerFactory.getLogger(CategoryController.class);
 
-    @GetMapping("/login")
+    @GetMapping("/addNewPosting")
     public String login() {
-        return "login";
+        return "addNewPosting";
     }
+
 }
