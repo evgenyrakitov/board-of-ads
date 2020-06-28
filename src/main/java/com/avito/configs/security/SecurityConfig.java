dea37f6050316c14b8071d2eae716c045c9a2992
RomanK <namor925@gmail.com>
2020-06-29 00:30:46 +0700

    fixed conflicts

--- src/main/java/com/avito/configs/security/SecurityConfig.java
+++ src/main/java/com/avito/configs/security/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/login/**", "/css/**", "/images/**", "/js/**", "/webjars/**", "/categories/**").permitAll()
+                .antMatchers("/", "/login/**", "/css/**", "/images/**", "/js/**", "/webjars/**", "/categories/**", "/rest/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
