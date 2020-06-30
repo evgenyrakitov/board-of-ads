244793765604dae37a5ffcb8a1f81cb5cc884419
Aleksandr Lysik <aleksandr.law@gmail.com>
2020-06-30 16:12:30 +0300

    Revert "удалил  регистрацию  и логин 300520 1407"
    This reverts commit b9a28e60a7bef0c16b3e8756de81b4a251a50a52.

---
+++ src/main/java/com/avito/controllers/simple/LoginController.java
@@ -0,0 +1,17 @@
+package com.avito.controllers.simple;
+
+import com.avito.configs.security.AuthProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
