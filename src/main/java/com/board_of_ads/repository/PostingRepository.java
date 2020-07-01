1b01c470c72ec8db41e0543c6b1f73b76002de55
timaogurtzova <48647083+timaogurtzova@users.noreply.github.com>
2020-07-01 11:46:20 +0300

    Убрала лишнюю аннотацию. Убрала зависимость-дубль из pom.xml

--- src/main/java/com/board_of_ads/repository/PostingRepository.java
+++ src/main/java/com/board_of_ads/repository/PostingRepository.java
@@ -3,11 +3,9 @@ package com.board_of_ads.repository;
 import com.board_of_ads.models.User;
 import com.board_of_ads.models.posting.Posting;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface PostingRepository extends JpaRepository<Posting, Long> {
 
     List<Posting> findAllByUser(User user);
