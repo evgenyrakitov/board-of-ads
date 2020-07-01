74f8ee8749d5d6deaf61a522ef8f36332fc829e6
Vadim Karpov <ksvkvv1990@gmail.com>
2020-07-02 00:43:53 +0300

    сброс пароля через почту

--- src/main/java/com/avito/configs/security/SecurityConfig.java
+++ src/main/java/com/avito/configs/security/SecurityConfig.java
@@ -60,7 +60,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/login/**", "/css/**", "/images/**", "/js/**", "/webjars/**", "/categories/**",  "/rest/admin/add").permitAll()
+                .antMatchers("/", "/login/**", "/css/**", "/images/**", "/js/**", "/webjars/**",
+                        "/categories/**",  "/rest/admin/add", "/user/**", "/updatePassword").permitAll()
                // .anyRequest().authenticated()
                 .and()
                 .formLogin()
