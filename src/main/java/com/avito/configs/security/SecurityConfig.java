80f9227ec3ae21d96592b1939672187eb9db9c6c
timaogurtzova <48647083+timaogurtzova@users.noreply.github.com>
2020-07-06 11:03:37 +0300

    В SecurityConfig добавлен "/rest/messages/**"

--- src/main/java/com/avito/configs/security/SecurityConfig.java
+++ src/main/java/com/avito/configs/security/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/login/**", "/css/**", "/images/**", "/js/**", "/webjars/**", "/categories/**",  "/rest/admin/add").permitAll()
+                .antMatchers("/", "/login/**", "/css/**", "/images/**", "/js/**", "/webjars/**", "/categories/**",  "/rest/admin/add", "/rest/messages/**").permitAll()
                 .anyRequest().authenticated();
 /*
                 .antMatchers("/", "/login/**", "/css/**", "/images/**", "/js/**", "/webjars/**",
