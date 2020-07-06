782a7daad2ab776dcb8d4f163874c1396ef1e94c
timaogurtzova <48647083+timaogurtzova@users.noreply.github.com>
2020-07-06 10:13:29 +0300

    Создан MessageRepository

---
+++ src/main/java/com/avito/repository/MessageRepository.java
@@ -0,0 +1,13 @@
+package com.avito.repository;
+
+import com.avito.models.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    @Query("SELECT count (m) FROM Message m WHERE m.readStatus = :readStatus AND m.posting.user.id = :id")
+    long countMessagesByReadStatusAndRecipientId(@Param("readStatus") Message.ReadStatus readStatus, @Param("id") long id);
+
+}
