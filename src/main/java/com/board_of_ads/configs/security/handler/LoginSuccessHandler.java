2e7f82ebe0d4abb4a8b6917e58a422dd6424b8ed
a3sma <xlsdff@gmail.com>
2020-07-07 19:29:58 +0300

    07.07.2020 Changes. Extra refactor. Made "user" global attribute as current SpringSecurity login user.

--- src/main/java/com/board_of_ads/configs/security/handler/LoginSuccessHandler.java
+++ src/main/java/com/board_of_ads/configs/security/handler/LoginSuccessHandler.java
@@ -3,8 +3,6 @@ package com.board_of_ads.configs.security.handler;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -15,7 +13,7 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
                                         HttpServletResponse httpServletResponse,
-                                        Authentication authentication) throws IOException, ServletException {
+                                        Authentication authentication) throws IOException {
         httpServletResponse.sendRedirect("/");
     }
 }
