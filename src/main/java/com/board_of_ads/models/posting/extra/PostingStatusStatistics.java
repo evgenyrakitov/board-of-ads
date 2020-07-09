b3de0d25756013f7f980f6d4720d16fdc774db13
a3sma <xlsdff@gmail.com>
2020-07-09 21:15:34 +0300

    2020.07.09 User profile postings tab.

--- src/main/java/com/board_of_ads/models/posting/extra/PostingStatusStatistics.java
+++ src/main/java/com/board_of_ads/models/posting/extra/PostingStatusStatistics.java
@@ -3,6 +3,11 @@ package com.board_of_ads.models.posting.extra;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * Класс нужен для получения информации по статусам объявления у пользователя.
+ * Какого статуса сколько штук. Эта информация нужна для страницы профиля пользователя вкладки "Мои объявления"
+ */
+
 @Data
 @AllArgsConstructor
 public class PostingStatusStatistics {
