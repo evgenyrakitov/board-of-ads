b43a568ea6d1ed7dfcc08a0c27dfc7c25511d775
Vadim Karpov <ksvkvv1990@gmail.com>
2020-07-16 23:48:54 +0300

    Сделать так чтобы на почту приходило письмо ввиде ссылки

--- src/main/java/com/board_of_ads/service/interfaces/EmailService.java
+++ src/main/java/com/board_of_ads/service/interfaces/EmailService.java
@@ -2,6 +2,11 @@ package com.board_of_ads.service.interfaces;
 
 import com.board_of_ads.models.User;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
     void sendMail(String subject, String body, User user);
+
+    void sendHTMLEmail(String subject, String bodyHTML, User user) throws MessagingException;
 }
