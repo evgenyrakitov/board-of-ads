59c1029b7a7e2220a47d20d7bd9ab828a71821e9
RomanK <namor925@gmail.com>
2020-07-19 22:35:21 +0700

    fixed and comment NotificationRestController

--- src/main/java/com/board_of_ads/controllers/rest/NotificationRestController.java
+++ src/main/java/com/board_of_ads/controllers/rest/NotificationRestController.java
@@ -1,19 +1,12 @@
 package com.board_of_ads.controllers.rest;
 
 import com.board_of_ads.models.Notification;
-import com.board_of_ads.models.User;
-import com.board_of_ads.service.interfaces.NotificationService;
-import com.google.gson.Gson;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/rest/notification")
@@ -22,8 +15,8 @@ public class NotificationRestController {
 
     private static final Logger logger = LoggerFactory.getLogger(RoleRestController.class);
 
-    private final NotificationService notificationService;
-
+    /*
+    * это тестовый контроллер по образу и подобию уже существующего для объявлений. функционала не несёт */
     public ResponseEntity<Notification> getNotification() {
         Notification notification = new Notification();
         notification.setTitle("Тестовая нотификация");
